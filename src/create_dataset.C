78e0231d8fb89acfea20b5874225cb20dd3af1ee
Sanjana <sanjanasekhar39@gmail.com>
2024-02-29 19:56:44 -0600

    fix bugs in create_dataset and add csv_files

--- src/create_dataset.C
+++ src/create_dataset.C
@@ -7,7 +7,7 @@
 #include "TH1.h"
 #include "TTree.h"
 #include "TLorentzVector.h"
-
+#include <cstdlib>
 #ifdef __CLING__
 R__LOAD_LIBRARY(libDelphes)
 #include "classes/DelphesClasses.h"
@@ -22,29 +22,33 @@ R__LOAD_LIBRARY(libDelphes)
 
 class TFile;
 
-void create_dataset() {
+int create_dataset(string file_n, int label) {
 
 
   int debug = 0;
   gSystem->Load("libDelphes");
-
-  bool isBkg = false;
+  //if(argc!=3) {
+  //	std::cout << "Please enter the root file name and the label ( 0 for bkg and 1 for signal )" << std::endl;
+  //	return 0;	
+  //}
+  int isSig = label;
 
   char infile[200], outfile[200];
-  
-  file_name = "LQ_nonResScalarLQ-M1000_2J";
-
-  sprintf(infile,"%s.root",file_name.c_str());
+  string csv_path = "/uscms/home/ssekhar/nobackup/CATHODE_ditau/";
+  string in_path = "root://cmseos.fnal.gov//store/user/tvami/diTauCathode/";
+  //string file_name = "LQ_nonResScalarLQ-M1000_2J";
+  string file_name = file_n.c_str();
+  sprintf(infile,"%s%s.root",in_path.c_str(),file_name.c_str());
   TFile * fin = TFile::Open(infile);
   FILE *fout;
-  sprintf(outfile,"csv_files/%s.csv",file_name.c_str());
+  sprintf(outfile,"%s/diTauCathode/csv_files/%s.csv",csv_path.c_str(),file_name.c_str());
   fout = fopen(outfile, "w");
   
   std::cout << "Sample used is " << file_name.c_str() << std::endl;
   
 
   TChain chain("Delphes");
-  chain.Add(file_name);
+  chain.Add(infile);
 //  chain.Add("/Users/tav/Software/MG5_aMC_v3_5_3/vbfHToTauTau-M750/Events/run_01/tag_1_delphes_events.root");
 
   ExRootTreeReader *treeReader = new ExRootTreeReader(&chain);
@@ -63,7 +67,7 @@ void create_dataset() {
   // TH1F histJetTauN("jetTau_phi", ";phi(j_{tau});Jets  / bin", 60, -3.2, 3.2);
 
   float tau1_pt, tau1_eta, tau1_phi, tau2_pt, tau2_eta, tau2_phi, tau1_m, tau2_m, m_tau1tau2;
-  int label;
+  
   
   std::cout << "Running on " << numberOfEntries << " events" << std::endl;
 
@@ -81,10 +85,10 @@ void create_dataset() {
 
     for (int i = 0; i < branchJet->GetEntries(); ++i) {
       Jet *jet = (Jet*) branchJet->At(i);
-      if (!jet and jet->TauTag != 1) continue;
+      if (!jet or jet->TauTag != 1) continue;
 
-      if(isBkg) label = 0 
-      else label = 1
+      //if(isBkg) label = 0; 
+      //else label = 1;
 
       if (jet->TauTag == 1) {
         ++numTauJets;
@@ -112,7 +116,33 @@ void create_dataset() {
         }
       }
     }
-    fprintf(fout,"%f %f %f %f %f %f %f %f %f %i\n", tau1_pt, tau1_eta, tau1_phi, tau2_pt, tau2_eta, tau2_phi, tau1_m, tau2_m, m_tau1tau2, label);
+    if(filledTau) fprintf(fout,"%f %f %f %f %f %f %f %f %f %i\n", tau1_pt, tau1_eta, tau1_phi, tau2_pt, tau2_eta, tau2_phi, tau1_m, tau2_m, m_tau1tau2, isSig);
+  }
+return 1;
 }
-
-
+/*
+ 150.386200 0.018794 -0.292659 0.000000 0.000000 -0.000000 0.841084 0.000000 0.000000 1
+248.562607 0.628225 -1.001127 0.000000 0.000000 -0.000000 14.215500 0.000000 0.000000 1
+384.063141 -1.317290 1.378457 0.000000 0.000000 -0.000000 2.655327 0.000000 0.000000 1
+384.063141 -1.317290 1.378457 0.000000 0.000000 -0.000000 2.655327 0.000000 0.000000 1
+384.063141 -1.317290 1.378457 0.000000 0.000000 -0.000000 2.655327 0.000000 0.000000 1
+409.222229 -0.750504 -1.340876 0.000000 0.000000 -0.000000 22.128954 0.000000 0.000000 1
+409.222229 -0.750504 -1.340876 0.000000 0.000000 -0.000000 22.128954 0.000000 0.000000 1
+409.222229 -0.750504 -1.340876 0.000000 0.000000 -0.000000 22.128954 0.000000 0.000000 1
+129.401672 1.598770 -2.875708 0.000000 0.000000 -0.000000 20.087402 0.000000 0.000000 1
+108.567940 1.533159 3.141070 108.567940 1.533159 3.141070 6.230235 6.230235 375.426056 1
+45.346210 1.502949 2.150964 108.567940 1.533159 3.141070 3.040915 6.230235 375.426056 1
+45.346210 1.502949 2.150964 108.567940 1.533159 3.141070 3.040915 6.230235 375.426056 1
+45.346210 1.502949 2.150964 108.567940 1.533159 3.141070 3.040915 6.230235 375.426056 1
+45.346210 1.502949 2.150964 108.567940 1.533159 3.141070 3.040915 6.230235 375.426056 1
+172.505829 0.576267 1.229211 172.505829 0.576267 1.229211 3.536976 3.536976 220.740326 1
+172.505829 0.576267 1.229211 172.505829 0.576267 1.229211 3.536976 3.536976 220.740326 1
+172.505829 0.576267 1.229211 172.505829 0.576267 1.229211 3.536976 3.536976 220.740326 1
+172.505829 0.576267 1.229211 172.505829 0.576267 1.229211 3.536976 3.536976 220.740326 1
+51.749622 0.121425 -0.717920 51.749622 0.121425 -0.717920 3.509698 3.509698 147.287445 1
+51.749622 0.121425 -0.717920 51.749622 0.121425 -0.717920 3.509698 3.509698 147.287445 1
+138.746414 2.359561 0.681335 51.749622 0.121425 -0.717920 8.624253 3.509698 147.287445 1
+138.746414 2.359561 0.681335 51.749622 0.121425 -0.717920 8.624253 3.509698 147.287445 1
+126.307266 0.391632 2.947564 51.749622 0.121425 -0.717920 3.220380 3.509698 147.287445 1
+126.307266 0.391632 2.947564 51.749622 0.121425 -0.717920 3.220380 3.509698 147.287445 1
+*/
