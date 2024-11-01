ff792827a2009d699d8377588defd8606dea4950
Sanjana Sekhar <sanjanasekhar39@gmail.com>
2024-10-31 22:50:58 -0400

    plotting updates

--- src/plotter.py
+++ src/plotter.py
@@ -7,37 +7,41 @@ import pandas as pd
 from sklearn.metrics import roc_curve, confusion_matrix
 from matplotlib.backends.backend_pdf import PdfPages
 
-def plot_features(sig,bkg1,bkg2,sig_label):
+def plot_features(sig,bkg,sig_label,bkg_labels):
 
 
 	# Format of csv file:
 	# tau1_pt, tau1_eta, tau1_phi, tau2_pt, tau2_eta, tau2_phi, tau1_m, 
 	# tau2_m, m_tau1tau2, met_met, met_eta, met_phi, n_jets, n_bjets, 
 	# jet1_pt, jet1_eta, jet1_phi, jet1_cef, jet1_nef, bjet1_pt, bjet1_eta, bjet1_phi, bjet1_cef, bjet1_nef, isSig
-    print(sig.shape, bkg1.shape, bkg2.shape)
+    print(sig.shape, bkg.shape)
 	
     sig.columns = ["m_jet1jet2", "deltaR_jet1jet2", "m_bjet1bjet2", "deltaR_bjet1bjet2", "deltaR_tau1tau2",
                     "tau1_pt", "tau1_eta", "tau1_phi", "tau2_pt", "tau2_eta", "tau2_phi", "tau1_m","tau2_m",
 		            "m_tau1tau2", "pt_tau1tau2", "eta_tau1tau2", "phi_tau1tau2","met_met", "met_eta", "met_phi", "n_jets", "n_bjets",
 			        "jet1_pt", "jet1_eta", "jet1_phi", "jet1_cef", "jet1_nef", "bjet1_pt", "bjet1_eta", "bjet1_phi", "bjet1_cef", "bjet1_nef",
-	                "jet2_pt", "jet2_eta", "jet2_phi", "jet2_cef", "jet2_nef", "bjet2_pt", "bjet2_eta", "bjet2_phi", "bjet2_cef", "bjet2_nef", "label"]
-    bkg1.columns = sig.columns
-    bkg2.columns = sig.columns
-    pp = PdfPages('plots/%s_DY_ttbar_distributions.pdf'%sig_label)
+	                "jet2_pt", "jet2_eta", "jet2_phi", "jet2_cef", "jet2_nef", "bjet2_pt", "bjet2_eta", "bjet2_phi", "bjet2_cef", "bjet2_nef", "event_weight","label"]
+    
+	for i in range(len(bkg)):
+		bkg[i].columns = sig.columns
+
+	sig = sig[["m_jet1jet2", "deltaR_jet1jet2","m_tau1tau2", "pt_tau1tau2","deltaR_tau1tau2","met_met","n_jets", "n_bjets", "event_weight"]]
+
+	for i in range(len(bkg)):
+		bkg[i].columns = sig.columns
+    
+    pp = PdfPages('plots/%s_DY_ttbar_QCD_distributions.pdf'%sig_label)
 
     for col in sig.columns:
-
 	    plt.figure(figsize=(10,7))
-	    plt.hist(bkg1[col], label = "DY + 0/1/2 jets", bins = 30, histtype = "step")
-	    plt.hist(bkg2[col], label = "ttbar + 0/1/2 jets", bins = 30, histtype = "step")
-	    plt.hist(sig[col], label = sig_label, bins = 30, histtype = "step")
+		for b, l in zip(bkg, bkg_labels):
+	    	plt.hist(b[col], label = l, bins = 30, histtype = "step", weights = b["event_weight"])
+	    plt.hist(sig[col], label = sig_label, bins = 30, histtype = "step", weights = sig["event_weight"])
 	    plt.legend()
 	    plt.title("Distribution of %s"%col)
 	    plt.xlabel(col)
-	    plt.yscale('log')
-	    #plt.ylim(np.amin([sig[col].min(),bkg1[col].min(),bkg2[col].min()]), np.amax([sig[col].max(),bkg1[col].max(),bkg2[col].max()])*10)
-	    plt.ylabel("No. of di-tau events (hadronic)")
-	    plt.xlim
+		plt.yscale('log')
+	    if 'm_' in col: plt.xlim(0,500)
         pp.savefig()
 	    plt.close()
 	
