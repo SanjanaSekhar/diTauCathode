97eea2f2512c0fa2eae4584e124ce3901ed52e2b
Sanjana <sanjanasekhar39@gmail.com>
2024-11-12 16:02:03 -0600

    mjj tautagged

--- src/plotter.py
+++ src/plotter.py
@@ -20,7 +20,7 @@ def plot_features(sigs,sig_labels,bkgs,bkg_labels):
 	# tau2_m, m_tau1tau2, met_met, met_eta, met_phi, n_jets, n_bjets, 
 	# jet1_pt, jet1_eta, jet1_phi, jet1_cef, jet1_nef, bjet1_pt, bjet1_eta, bjet1_phi, bjet1_cef, bjet1_nef, isSig
 
-    m_sig = ROOT.TH1F("m_sig", "m_sig", 20, 0.0, 100.0)
+    m_sig = ROOT.TH1F("m_sig", "m_sig", 80, 0.0, 300.0)
     delta_sig = ROOT.TH1F("delta_sig","delta_sig",20, 0, 5)
     n_sig = ROOT.TH1F("n_sig","n_sig", 10,0,10)
     m_bkg, delta_bkg, n_bkg = [],[],[]
