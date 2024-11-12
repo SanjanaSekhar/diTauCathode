b0e049c1e0779b5376621f728f3e9499931272ff
Sanjana Sekhar <sanjanasekhar39@gmail.com>
2024-11-12 18:48:41 -0500

    different legs

--- src/plotter.py
+++ src/plotter.py
@@ -191,8 +191,8 @@ def plot_features(sigs,sig_labels,bkgs,bkg_labels):
 				leg_n.Draw()
 
 			if idx==0: c.Print("plots/"+sig__+".pdf(")
-            elif idx==len(sig.columns-2): c.Print("plots/"+sig__+".pdf)")
-            else: c.Print("plots/"+sig__+".pdf")
+			elif idx==len(sig.columns-2): c.Print("plots/"+sig__+".pdf)")
+			else: c.Print("plots/"+sig__+".pdf")
 			
 
 def plot_pre_postprocessed(train, val, test, train_ws, val_ws, test_ws):
@@ -365,7 +365,7 @@ sig_list = [#"2HDM-vbfPhiToTauTau-M250_2J_MinMass120_NoMisTag",
 
 bkg_list = ["SM_QCD_JJ_0J1J2J_MinMass120_LO_6M", "SM_ttbarTo2Tau2Nu_0J1J2J_MinMass120_MadSpin_2M", "SM_dyToTauTau_0J1J2J_MinMass120_3M"]
 sig_names = [#"250 GeV heavy Higgs (VBF)", "250 GeV scalar from T'", "250 GeV HNL", 
-                "250 GeV VAL"]
+				"250 GeV VAL"]
 bkg_names = ["QCD multijet", "ttbar + 0/1/2 jets", "DY + 0/1/2 jets"]
 '''
 bkg = []
