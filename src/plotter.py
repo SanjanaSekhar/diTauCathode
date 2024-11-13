f0f908d041db409c161127ebe57c1132e87ff802
Sanjana <sanjanasekhar39@gmail.com>
2024-11-12 18:09:21 -0600

    checking pdf

--- src/plotter.py
+++ src/plotter.py
@@ -358,14 +358,14 @@ def plot_ROC_SIC(ws_lists, ws_names, fs_lists, fs_names, plt_title):
 	plt.savefig("plots/SIC_%s.png"%plt_title)
 	plt.close()
 
-sig_list = [#"2HDM-vbfPhiToTauTau-M250_2J_MinMass120_NoMisTag",
+sig_list = ["2HDM-vbfPhiToTauTau-M250_2J_MinMass120_NoMisTag"]
 			#"eVLQ_TPrimeTPrimeToTTPhiPhiToTauTauAll_TpM1000_PhiM250_NoMisTag",
 			#"HeavyN_vbsNToTauTau_NM250_2J_LO", 
-			"VAL_dyVfVfToXiCXiCToTauSTauS_XiM1000_VfM250_MinMass120_NoMisTag"]
+			#"VAL_dyVfVfToXiCXiCToTauSTauS_XiM1000_VfM250_MinMass120_NoMisTag"]
 
 bkg_list = ["SM_QCD_JJ_0J1J2J_MinMass120_LO_6M", "SM_ttbarTo2Tau2Nu_0J1J2J_MinMass120_MadSpin_2M", "SM_dyToTauTau_0J1J2J_MinMass120_3M"]
-sig_names = [#"250 GeV heavy Higgs (VBF)", "250 GeV scalar from T'", "250 GeV HNL", 
-				"250 GeV VAL"]
+sig_names = ["250 GeV heavy Higgs (VBF)"]#, "250 GeV scalar from T'", "250 GeV HNL", 
+				#"250 GeV VAL"]
 bkg_names = ["QCD multijet", "ttbar + 0/1/2 jets", "DY + 0/1/2 jets"]
 '''
 bkg = []
