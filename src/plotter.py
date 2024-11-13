e94d83b2aa0619cba943e9a3230c0a8371fb0bdb
Sanjana Sekhar <sanjanasekhar39@gmail.com>
2024-11-13 17:02:08 -0500

    why are the delta hists wrong

--- src/plotter.py
+++ src/plotter.py
@@ -199,6 +199,11 @@ def plot_features(sigs,sig_labels,bkgs,bkg_labels, sig_scale,plot_label):
 			if idx==0: c.Print("plots/"+sig__+"%s.pdf("%plot_label)
 			elif idx==len(sig.columns)-2: c.Print("plots/"+sig__+"%s.pdf)"%plot_label)
 			else: c.Print("plots/"+sig__+"%s.pdf"%plot_label)
+
+			stack_mtt.Delete()
+			stack.Delete()
+			stack_delta.Delete()
+			stack_n.Delete()
 			
 
 def plot_pre_postprocessed(train, val, test, train_ws, val_ws, test_ws):
