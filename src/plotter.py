52ec414603dede66ec2886fcfe0b19d22eb6cbe4
Sanjana <sanjanasekhar39@gmail.com>
2024-11-21 12:20:19 -0600

    after applying offline cuts

--- src/plotter.py
+++ src/plotter.py
@@ -150,8 +150,8 @@ def plot_features(sigs,sig_labels,bkgs,bkg_labels,sig_scale,plot_label):
 	# tau2_m, m_tau1tau2, met_met, met_eta, met_phi, n_jets, n_bjets, 
 	# jet1_pt, jet1_eta, jet1_phi, jet1_cef, jet1_nef, bjet1_pt, bjet1_eta, bjet1_phi, bjet1_cef, bjet1_nef, isSig
 
-	m_sig = ROOT.TH1F("m_sig", "m_sig", 60, 0.0, 600.0)
-	m_tt_sig = ROOT.TH1F("m_tt_sig", "m_tt_sig", 100, 0.0, 1000.0)
+	m_sig = ROOT.TH1F("m_sig", "m_sig", 70, 0.0, 700.0)
+	m_tt_sig = ROOT.TH1F("m_tt_sig", "m_tt_sig", 90, 100, 1000.0)
 	delta_sig = ROOT.TH1F("delta_sig","delta_sig",30, 0, 5)
 	n_sig = ROOT.TH1F("n_sig","n_sig",7,0,7)
 	m_bkg, m_tt_bkg, delta_bkg, n_bkg = [],[],[],[]
@@ -186,7 +186,7 @@ def plot_features(sigs,sig_labels,bkgs,bkg_labels,sig_scale,plot_label):
 		#bkg[i]["deltaeta_tau1tau2"] = abs(bkg[i]['tau1_eta'] - bkg[i]['tau2_eta'])
 		bkg[i] = bkg[i][["deltaR_jet1jet2", "deltaeta_tau1tau2","deltaR_tau1tau2","n_jets", "n_bjets","m_tau1tau2", "m_jet1jet2", "pt_tau1tau2", "pt_jet1jet2","met_met",  "event_weight"]]
 
-	#bkg[0]["event_weight"] = bkg[0]["event_weight"] * 0.01
+	bkg[0]["n_jets"] = bkg[0]["n_jets"] -2
 	#print("mjj in QCD: ",bkg[2]["m_jet1jet2"].min(),bkg[2]["m_jet1jet2"].max()) 
 	#print("deltaR_tau1tau2 in QCD: ",bkg[2]["deltaR_tau1tau2"].min(),bkg[2]["deltaR_tau1tau2"].max())
 
@@ -506,7 +506,7 @@ sig_names = ["250 GeV heavy Higgs (VBF)"]#, "250 GeV scalar from T'", "250 GeV H
 				#"250 GeV VAL"]
 bkg_names = ["QCD multijet", "ttbar + 0/1/2 jets", "DY + 0/1/2 jets"]
 #bkg_names = ["QCD multijet", "QCD multijet (tautagged)"]
-plot_features(sig_list, sig_names, bkg_list, bkg_names, sig_scale = 50, plot_label = "")
+plot_features(sig_list, sig_names, bkg_list, bkg_names, sig_scale = 30, plot_label = "")
 
 bkg_list = ["SM_QCD_JJ_0J1J2J_MinMass120_LO_6M","SM_QCD_JJ_0J1J2J_MinMass120_LO_6M_TauTag"]
 bkg_names = ["QCD multijet", "QCD multijet (tautagged)"]
